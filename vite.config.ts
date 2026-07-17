6ec66af246c0d06626b4320728b8324a3a1dce0b
unknown <ammarsk200422@gmail.com>
2026-07-17 18:59:01 +0530

    Deleted duplicate vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
     plugins: [react()],
-    base: "/ammar-portfolio/", // Ye exact path hona zaroori hai
+    base: "/ammar-portfolio/", 
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
