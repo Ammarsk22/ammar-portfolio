0d93f2c862f7ac9f9fa8e828bf13274aa4cbcc4d
unknown <ammarsk200422@gmail.com>
2026-07-17 18:30:50 +0530

    Fixed favicon path and vite base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [react()],
+    base: "/ammar-portfolio/",
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
 });
